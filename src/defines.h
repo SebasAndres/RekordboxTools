8990bd325eb99f165778656f46c8aedc7c809428
Sebastian Ignacio Andres <sebastian.ignacio.andres@gmail.com>
2024-11-09 14:46:12 -0300

    pruebas

---
+++ src/defines.h
@@ -0,0 +1,6 @@
+#define MAX_FILES 6000
+
+#define DST_FOLDER "/media/sebastian/Elements/Tracks"//"files/Tracks"
+#define CONTENTS_FOLDER_PATH "/media/sebastian/Elements/pen_conflicto/Contents" //"files/Contents"
+
+#define ALLOWED_EXTENSIONS {".mp3", ".wav", ".txt"}
