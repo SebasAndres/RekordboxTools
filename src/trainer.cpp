38350a2ba6c00752d5371fd156ddd3a0ddf561b4
Sebastian Ignacio Andres <sebastian.ignacio.andres@gmail.com>
2024-11-10 00:45:01 -0300

    ah

--- src/trainer.cpp
+++ src/trainer.cpp
@@ -5,7 +5,6 @@ Trainer::Trainer(fs::path src, fs::path dst) : Functionality(src, dst) {
 }
 
 void Trainer::execute(){
-      cout << "dale pa, entrena el modelo !! :)" << endl;
       string command = "python3 train.py -dataset " + (string)this->src + " -results " + (string)this->dst; 
       int result = system(command.c_str());
       if(result) {
